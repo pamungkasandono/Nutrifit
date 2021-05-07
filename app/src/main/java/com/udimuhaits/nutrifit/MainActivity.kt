d9f1eeadcc81b29f398b4dade84aa920f4d0ae31
pamungkas andono <pamungkas.org@gmail.com>
2021-05-07 12:33:04 +0700

    Initial commit

---
+++ app/src/main/java/com/udimuhaits/nutrifit/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.udimuhaits.nutrifit
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.udimuhaits.nutrifit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private var isBackPressed = false
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnSearch.setOnClickListener {
+            startActivity(Intent(this, DetailActivity::class.java))
+        }
+    }
+
+    override fun onBackPressed() {
+        if (isBackPressed) {
+            super.onBackPressed()
+        }
+
+        isBackPressed = true
+        Toast.makeText(this, "Tekan sekali lagi untuk kembali", Toast.LENGTH_SHORT).show()
+        Handler().postDelayed(Runnable { isBackPressed = false }, 2000)
+    }
+}
---
+++ app/src/main/java/com/udimuhaits/nutrifit/Splash.kt
@@ -0,0 +1,30 @@
+package com.udimuhaits.nutrifit
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+
+@Suppress("DEPRECATION")
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
