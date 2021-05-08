cc30be42c6af8518cd6187bc03fc73d2b77d704e
Zaki <abdanzaki07@gmail.com>
2021-05-09 00:41:48 +0700

    sharedpreferences

--- app/src/main/java/com/udimuhaits/nutrifit/SplashActivity.kt
+++ app/src/main/java/com/udimuhaits/nutrifit/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.udimuhaits.nutrifit
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
@@ -9,9 +11,25 @@ import androidx.appcompat.app.AppCompatDelegate
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE)
+
+        val isOpened = sharedPreferences.getBoolean("isOpened", false)
+        if (!isOpened) {
+            sharedPreferences.edit().apply {
+                putBoolean("isOpened", true)
+                navigateToContainer()
+                apply()
+            }
+        } else {
+            navigateToLogin()
+        }
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
@@ -21,9 +39,19 @@ class SplashActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+    }
 
+    private fun navigateToContainer() {
         Handler().postDelayed({
             val intent = Intent(this, ContainerActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+
+    private fun navigateToLogin() {
+        Handler().postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 2000)
